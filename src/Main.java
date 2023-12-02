a8c0092c14580b71da3336fefcffeb979e30cee7
Alexander Sandberg <Alexandersandbergnya@gmail.com>
2023-12-02 22:10:43 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       // Skapa ett program som skriver ut kvadraten på ett tal som läses in. Kvadrat är talet
+       // multiplicerat med sig själv.
+        int tal =4;
+        System.out.println("\nSkapa ett program som skriver ut kvadraten på ett tal som läses in. Kvadrat är talet\n" +
+                "multiplicerat med sig själv."+
+tal*tal);
+
+
+        }
+    }
